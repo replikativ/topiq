6cb668f96ddaf8f443b4b900b49d2a4e0ebb8ac8
kordano <konrad.kuehne@rocketmail.com>
2014-05-23 14:10:35 +0200

    initial status

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to collective
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/collective/core_test.clj
@@ -0,0 +1,7 @@
+(ns collective.core-test
+  (:require [clojure.test :refer :all]
+            [collective.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
