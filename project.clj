82ef5730cb2ba092126e3d5f6ab4b944a82000a9
Christian Weilbach <christian_weilbach@web.de>
2014-05-26 18:40:34 +0200

    Some demo stage data for synching.

--- project.clj
+++ project.clj
@@ -1,10 +1,42 @@
 (defproject link-collective "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
+  :description "An app to collectively link and share data."
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/kordano/link-collective"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
+
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.7.6"]
+                 [org.clojure/tools.logging "0.2.6"]
+
+                 [ring "1.2.2"]
+                 [com.cemerick/friend "0.2.0"]
+                 [com.ashafa/clutch "0.4.0-RC1"]
+                 [enlive "1.1.5"]
+                 [compojure "1.1.6"]
+
+                 [datascript "0.1.4"]
+                 [om "0.5.0"]
+                 [kioo "0.4.0"]
+                 [http-kit "2.1.18"]
+                 [com.facebook/react "0.9.0.1"]
+                 [net.polyc0l0r/geschichte "0.1.0-SNAPSHOT"]
+
+                 [weasel "0.2.0"]]
+
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  :cljsbuild
+  {:builds
+   [{:source-paths ["src/cljs"]
+     :compiler
+     {:output-to "resources/public/js/main.js"
+      :optimizations :simple}}]})
--- src/cljs/link_collective/core.cljs
+++ src/cljs/link_collective/core.cljs
@@ -1,1 +1,159 @@
-(ns link-collective.core)
+(ns link-collective.core
+  (:require [weasel.repl :as ws-repl]
+            [hasch.core :refer [uuid]]
+            [datascript :as d]
+            [geschichte.stage :as s]
+            [geschichte.sync :refer [client-peer]]
+            [konserve.store :refer [new-mem-store]]
+            [cljs.core.async :refer [put! chan <! >! alts! timeout close!] :as async]
+            [kioo.om :refer [content set-attr do-> substitute listen]]
+            [kioo.core :refer [handle-wrapper]]
+            [om.core :as om :include-macros true])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+
+(enable-console-print!)
+
+;; fire up repl
+#_(do
+    (ns weasel.startup)
+    (require 'weasel.repl.websocket)
+    (cemerick.piggieback/cljs-repl
+        :repl-env (weasel.repl.websocket/repl-env
+                   :ip "0.0.0.0" :port 17782)))
+
+;; todo
+;; - implement data model in repo
+;;   - fix realize value
+;; - load in templates
+
+(ws-repl/connect "ws://localhost:17782" :verbose true)
+
+(.log js/console "HAIL TO THE LAMBDA!")
+
+(def eval-fn {'(fn replace [old params] params) (fn replace [old params] params)
+              '(fn [old params]
+                 (merge-with merge old params))
+              (fn [old params]
+                (merge-with merge old params))})
+
+(def val-ch (chan))
+
+
+(let [schema {:aka {:db/cardinality :db.cardinality/many}}
+      conn   (d/create-conn schema)]
+  (:db-after (d/transact @conn [ { :db/id -1
+                                  :name  "Maksim"
+                                  :age   45
+                                  :aka   ["Maks Otto von Stirlitz", "Jack Ryan"] } ]))
+  #_(d/q '[ :find  ?n ?a
+          :where [?e :aka "Maks Otto von Stirlitz"]
+                 [?e :name ?n]
+                 [?e :age  ?a] ]
+       @conn))
+
+
+
+#_(go (def store
+        (<! (new-mem-store
+             (atom {#uuid "0912a672-6bc2-5297-9ffa-948998517273"
+                    {:transactions
+                     [[#uuid "2b21fbe0-e8a8-563d-bfba-e4b6022d056f"
+                       #uuid "123ed64b-1e25-59fc-8c5b-038636ae6c3d"]],
+                     :parents [],
+                     :ts #inst "2014-05-24T19:14:16.158-00:00",
+                     :author "eve@polyc0l0r.net"},
+                    #uuid "123ed64b-1e25-59fc-8c5b-038636ae6c3d"
+                    '(fn replace [old params] params),
+                    "eve@polyc0l0r.net"
+                    {#uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
+                     {:description "link-collective discourse.",
+                      :schema {:type "http://github.com/ghubber/geschichte", :version 1},
+                      :pull-requests {},
+                      :causal-order {#uuid "0912a672-6bc2-5297-9ffa-948998517273" []},
+                      :public false,
+                      :branches
+                      {"master" #{#uuid "0912a672-6bc2-5297-9ffa-948998517273"}},
+                      :head "master",
+                      :last-update #inst "2014-05-24T19:14:16.158-00:00",
+                      :id #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"}},
+                    #uuid "2b21fbe0-e8a8-563d-bfba-e4b6022d056f"
+                    {:posts
+                     {#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"
+                      {:title "Spiegel Online",
+                       :content "http://spiegel.de #spon",
+                       :author "kordano",
+                       :ts #inst "2014-05-24T19:14:16.092-00:00"}},
+                     :comments
+                     {#uuid "098fc269-d8c1-524d-a049-9a8cc52d6268"
+                      {:content "this is boring :-/",
+                       :author "bob@polyc0l0r.net",
+                       :ts #inst "2014-05-24T19:14:16.120-00:00"}},
+                     :posts->comments
+                     {#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"
+                      #{#uuid "098fc269-d8c1-524d-a049-9a8cc52d6268"}},
+                     :hashtags->posts
+                     {#uuid "17d34c1b-0535-5a5b-8c2a-2113cae5aca3"
+                      #{#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"}},
+                     :posts->votes
+                     {#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"
+                      #{#uuid "254995fc-f837-530a-a4fa-a2923dc6db4a"}},
+                     :hashtags {#uuid "17d34c1b-0535-5a5b-8c2a-2113cae5aca3" :#spon},
+                     :votes
+                     {#uuid "254995fc-f837-530a-a4fa-a2923dc6db4a"
+                      {:author "bob@polyc0l0r.net", :type :down}}}}))))
+
+
+      (def peer (client-peer "CLIENT-PEER" store))
+
+      (def stage (<! (s/create-stage! "eve@polyc0l0r.net" peer eval-fn)))
+
+      (async/tap (get-in @stage [:volatile :val-mult]) val-ch))
+
+
+
+#_(let [post {:title "Spiegel Online"
+            :content "http://spiegel.de #spon"
+            :author "kordano"
+            :ts (js/Date.)}
+      post-id (uuid post)
+      comment {:content "this is boring :-/"
+               :author "bob@polyc0l0r.net"
+               :ts (js/Date.)}
+      comment-id (uuid comment)
+      vote {:author "bob@polyc0l0r.net" :type :down}
+      vote-id (uuid vote)]
+  (go (<! (s/create-repo! stage "eve@polyc0l0r.net" "link-collective discourse."
+                          {:posts {post-id post}
+                           :hashtags {(uuid :#spon) :#spon}
+                           :hashtags->posts {(uuid :#spon) #{post-id}}
+                           :comments {comment-id comment}
+                           :posts->comments {post-id #{comment-id}}
+                           :votes {vote-id vote}
+                           :posts->votes {post-id #{vote-id}}}
+                          "master"))))
+
+
+(comment
+  (s/subscribe-repos! stage
+                      {"eve@polyc0l0r.net" {#uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
+                                            #{"master"}}})
+
+  (get-in @stage ["eve@polyc0l0r.net" #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"])
+  (get-in @stage [:volatile :val
+                  "eve@polyc0l0r.net" #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363" "master"])
+  (get-in @stage [:volatile :val])
+
+  (let [post {:title "Netzwertig"
+              :content "http://netzwertig.de"
+              :ts (js/Date.)}
+        post-id (uuid post)]
+    (go (<! (s/transact stage ["eve@polyc0l0r.net"
+                               #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
+                               "master"]
+                        {:posts {post-id post}}
+                        '(fn [old params]
+                           (merge-with merge old params))))))
+
+  (go (<! (s/commit! stage {"eve@polyc0l0r.net" {#uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
+                                                 #{"master"}}}))))
