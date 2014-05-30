e1fee816f23d6aa21b5b9e84a88135f30a33e4b2
Christian Weilbach <christian_weilbach@web.de>
2014-05-30 17:43:55 +0200

    Use datascript as repo value.

--- src/cljs/link_collective/core.cljs
+++ src/cljs/link_collective/core.cljs
@@ -6,7 +6,7 @@
             [geschichte.sync :refer [client-peer]]
             [konserve.store :refer [new-mem-store]]
             [cljs.core.async :refer [put! chan <! >! alts! timeout close!] :as async]
-            [cljs.reader :refer [read-string]]
+            [cljs.reader :refer [read-string] :as read]
             [kioo.om :refer [content set-attr do-> substitute listen]]
             [kioo.core :refer [handle-wrapper]]
             [om.core :as om :include-macros true])
@@ -24,8 +24,6 @@
                    :ip "0.0.0.0" :port 17782)))
 
 ;; todo
-;; - implement data model in repo
-;;   - fix realize value
 ;; - load in templates
 
 (ws-repl/connect "ws://localhost:17782" :verbose true)
@@ -34,148 +32,82 @@
 
 (def eval-fn {'(fn replace [old params] params) (fn replace [old params] params)
               '(fn [old params]
-                 (merge-with merge old params))
+                 (:db-after (d/transact old params)))
               (fn [old params]
-                (merge-with merge old params))})
+                (:db-after (d/transact old params)))})
 
 (def val-ch (chan))
 
-
-#_(let [post {:title "Spiegel Online"
-            :content "http://spiegel.de #spon"
-            :author "kordano"
-            :ts (js/Date.)}
-      post-id (uuid post)
-      comment {:content "this is boring :-/"
-               :author "bob@polyc0l0r.net"
-               :ts (js/Date.)}
-      comment-id (uuid comment)
-      vote {:author "bob@polyc0l0r.net" :type :down}
-      vote-id (uuid vote)]
-  (go (<! (s/create-repo! stage "eve@polyc0l0r.net" "link-collective discourse."
-                          {:posts {post-id post}
-                           :hashtags {(uuid :#spon) :#spon}
-                           :hashtags->posts {(uuid :#spon) #{post-id}}
-                           :comments {comment-id comment}
-                           :posts->comments {post-id #{comment-id}}
-                           :votes {vote-id vote}
-                           :posts->votes {post-id #{vote-id}}}
-                          "master"))))
-
-
-(let [schema {:up-votes {:db/cardinality :db.cardinality/many}
-              :down-votes {:db/cardinality :db.cardinality/many}
-              :comments {:db/cardinality :db.cardinality/many}
-              :hashtags {:db/cardinality :db.cardinality/many}}
-      conn   (d/create-conn schema)
-      init-db (:db-after (d/transact @conn [{:db/id -1
-                                             :title "Spiegel Online"
-                                             :content "http://spiegel.de #spon"
-                                             :author "bob@polyc0l0r.net"
-                                             :ts (js/Date.)
-                                             :hashtags #{:#spon}
-                                             :comments #{}
-                                             :votes #{{:author "kordano@polyc0l0r.net"
-                                                       :type :down}}}
-                                            {:db/id 1
-                                             :content "this is boring :-/"
-                                             :up-votes #{"kordano@polyc0l0r.net"}
-                                             :down-votes #{"bob@polyc0l0r.net"}
-                                             :author "kordano@polyc0l0r.net"
-                                             :ts (js/Date.)}]))]
-  #_(hasch.platform/as-value init-db)
-  #_(go (<! (s/create-repo! stage
+#_(let [schema {:up-votes {:db/cardinality :db.cardinality/many}
+                :down-votes {:db/cardinality :db.cardinality/many}
+                :comments {:db/cardinality :db.cardinality/many}
+                :hashtags {:db/cardinality :db.cardinality/many}}
+        conn   (d/create-conn schema)]
+    (go (<! (s/create-repo! stage
                             "eve@polyc0l0r.net"
                             "link-collective discourse."
-                            init-db
-                            "master")))
-  init-db
-  #_(d/q '[:find ?c ?e ?v
-         :where
-         [?e :author "bob@polyc0l0r.net"]
-         [?e :comments ?c]
-         [?c :up-votes ?v]]
-       init-db))
+                            @conn
+                            "master"))))
 
-#_(go (def store
-        (<! (new-mem-store
-             #_(atom {#uuid "0912a672-6bc2-5297-9ffa-948998517273"
-                    {:transactions
-                     [[#uuid "2b21fbe0-e8a8-563d-bfba-e4b6022d056f"
-                       #uuid "123ed64b-1e25-59fc-8c5b-038636ae6c3d"]],
-                     :parents [],
-                     :ts #inst "2014-05-24T19:14:16.158-00:00",
-                     :author "eve@polyc0l0r.net"},
-                    #uuid "123ed64b-1e25-59fc-8c5b-038636ae6c3d"
-                    '(fn replace [old params] params),
-                    "eve@polyc0l0r.net"
-                    {#uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
-                     {:description "link-collective discourse.",
-                      :schema {:type "http://github.com/ghubber/geschichte", :version 1},
-                      :pull-requests {},
-                      :causal-order {#uuid "0912a672-6bc2-5297-9ffa-948998517273" []},
-                      :public false,
-                      :branches
-                      {"master" #{#uuid "0912a672-6bc2-5297-9ffa-948998517273"}},
-                      :head "master",
-                      :last-update #inst "2014-05-24T19:14:16.158-00:00",
-                      :id #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"}},
-                    #uuid "2b21fbe0-e8a8-563d-bfba-e4b6022d056f"
-                    {:posts
-                     {#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"
-                      {:title "Spiegel Online",
-                       :content "http://spiegel.de #spon",
-                       :author "kordano",
-                       :ts #inst "2014-05-24T19:14:16.092-00:00"}},
-                     :comments
-                     {#uuid "098fc269-d8c1-524d-a049-9a8cc52d6268"
-                      {:content "this is boring :-/",
-                       :author "bob@polyc0l0r.net",
-                       :ts #inst "2014-05-24T19:14:16.120-00:00"}},
-                     :posts->comments
-                     {#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"
-                      #{#uuid "098fc269-d8c1-524d-a049-9a8cc52d6268"}},
-                     :hashtags->posts
-                     {#uuid "17d34c1b-0535-5a5b-8c2a-2113cae5aca3"
-                      #{#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"}},
-                     :posts->votes
-                     {#uuid "164564fb-f93b-5863-9ee2-4517a42d0e99"
-                      #{#uuid "254995fc-f837-530a-a4fa-a2923dc6db4a"}},
-                     :hashtags {#uuid "17d34c1b-0535-5a5b-8c2a-2113cae5aca3" :#spon},
-                     :votes
-                     {#uuid "254995fc-f837-530a-a4fa-a2923dc6db4a"
-                      {:author "bob@polyc0l0r.net", :type :down}}}}))))
+(read/register-tag-parser! 'datascript.DB datascript/map->DB)
+(read/register-tag-parser! 'datascript.Datom datascript/map->Datom)
 
 
-      (def peer (client-peer "CLIENT-PEER" store))
+#_(go
+    (def store
+      (<! (new-mem-store
+           (atom (read-string "{#uuid \"029f3ec1-f6f9-5398-8507-3612cb99e109\" #datascript.DB{:schema {:up-votes {:db/cardinality :db.cardinality/many}, :down-votes {:db/cardinality :db.cardinality/many}, :comments {:db/cardinality :db.cardinality/many}, :hashtags {:db/cardinality :db.cardinality/many}}, :ea {}, :av {}, :max-eid 0, :max-tx 536870912}, #uuid \"123ed64b-1e25-59fc-8c5b-038636ae6c3d\" (fn replace [old params] params), #uuid \"2cab7867-38ef-5421-be45-461eb7619b61\" {:transactions [[#uuid \"029f3ec1-f6f9-5398-8507-3612cb99e109\" #uuid \"123ed64b-1e25-59fc-8c5b-038636ae6c3d\"]], :parents [], :ts #inst \"2014-05-30T15:20:53.536-00:00\", :author \"eve@polyc0l0r.net\"}, \"eve@polyc0l0r.net\" {#uuid \"fc9f725a-20eb-42f1-bb27-80d6fb2d1945\" {:branches {\"master\" #{#uuid \"2cab7867-38ef-5421-be45-461eb7619b61\"}}, :id #uuid \"fc9f725a-20eb-42f1-bb27-80d6fb2d1945\", :description \"link-collective discourse.\", :head \"master\", :last-update #inst \"2014-05-30T15:20:53.536-00:00\", :schema {:type \"http://github.com/ghubber/geschichte\", :version 1}, :causal-order {#uuid \"2cab7867-38ef-5421-be45-461eb7619b61\" []}, :public false, :pull-requests {}}}}")))))
 
-      (def stage (<! (s/create-stage! "eve@polyc0l0r.net" peer eval-fn)))
+    (def peer (client-peer "CLIENT-PEER" store))
 
-      #_(async/tap (get-in @stage [:volatile :val-mult]) val-ch))
+    (def stage (<! (s/create-stage! "eve@polyc0l0r.net" peer eval-fn)))
+
+    (async/tap (get-in @stage [:volatile :val-mult]) val-ch))
 
 
 
 
 (comment
   (s/subscribe-repos! stage
-                      {"eve@polyc0l0r.net" {#uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
+                      {"eve@polyc0l0r.net" {#uuid "fc9f725a-20eb-42f1-bb27-80d6fb2d1945"
                                             #{"master"}}})
 
-  (get-in @stage ["eve@polyc0l0r.net" #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"])
-  (get-in @stage [:volatile :val
-                  "eve@polyc0l0r.net" #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363" "master"])
-  (get-in @stage [:volatile :val])
+  (get-in @stage ["eve@polyc0l0r.net" #uuid "fc9f725a-20eb-42f1-bb27-80d6fb2d1945"])
 
-  (let [post {:title "Netzwertig"
-              :content "http://netzwertig.de"
-              :ts (js/Date.)}
-        post-id (uuid post)]
-    (go (<! (s/transact stage ["eve@polyc0l0r.net"
-                               #uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
-                               "master"]
-                        {:posts {post-id post}}
+  (get-in @stage [:volatile :val])
+  (let [post-id (uuid)
+        comment-id1 (uuid)
+        comment-id2 (uuid)
+        ts (js/Date.)]
+    (go (<! (s/transact stage
+                        ["eve@polyc0l0r.net"
+                         #uuid "fc9f725a-20eb-42f1-bb27-80d6fb2d1945"
+                         "master"]
+                        [{:db/id post-id
+                          :title "Spiegel Online"
+                          :content "http://spiegel.de #spon"
+                          :author "bob@polyc0l0r.net"
+                          :ts ts}
+                         {:db/id post-id
+                          :comments comment-id1}
+                         {:db/id post-id
+                          :comments comment-id2}
+                         {:db/id comment-id1
+                          :content "this is boring :-/"
+                          :author "kordano@polyc0l0r.net"
+                          :ts ts}
+                         {:db/id comment-id2
+                          :content "i like pizza"
+                          :author "joe@polyc0l0r.net"
+                          :ts ts}]
                         '(fn [old params]
-                           (merge-with merge old params))))))
+                           (:db-after (d/transact old params)))))))
+
+  (d/q '[:find ?e
+         :where
+         [?e :author "bob@polyc0l0r.net"]]
+       (get-in @stage [:volatile :val
+                       "eve@polyc0l0r.net" #uuid "fc9f725a-20eb-42f1-bb27-80d6fb2d1945" "master"]))
 
   (go (<! (s/commit! stage {"eve@polyc0l0r.net" {#uuid "1bc987e2-f19e-4f6a-9341-8858ad4d4363"
                                                  #{"master"}}}))))
