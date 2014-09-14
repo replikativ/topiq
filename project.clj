a05d7df1d0f9ca3ed7b09a07c7995f0ddf12b535
Christian Weilbach <christian_weilbach@web.de>
2014-09-14 19:38:41 +0200

    Bump dependency versions.

--- project.clj
+++ project.clj
@@ -8,32 +8,33 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"]
-                 [org.clojure/core.cache "0.6.3"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [org.clojure/core.cache "0.6.4"]
                  [org.clojure/core.memoize "0.5.6" :exclusions [org.clojure/core.cache]]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
 
-                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
-                 [org.slf4j/slf4j-log4j12 "1.7.6"]
-                 [org.clojure/tools.logging "0.2.6"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms
+                                                    com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.7.7"]
+                 [org.clojure/tools.logging "0.3.0"]
 
-                 [http-kit "2.1.18"]
-                 [ring "1.2.2"]
+                 [http-kit "2.1.19"]
+                 [ring "1.3.1"]
                  [com.cemerick/friend "0.2.1"]
                  [enlive "1.1.5"]
-                 [compojure "1.1.6"]
+                 [compojure "1.1.9"]
 
                  [domina "1.0.2"]
-                 [prismatic/dommy "0.1.2"]
-                 [datascript "0.1.6"]
-                 [om "0.6.3"]
+                 [prismatic/dommy "0.1.3"]
+                 [datascript "0.4.0"]
+                 [om "0.7.3"]
                  [kioo "0.4.0"]
                  [figwheel "0.1.3-SNAPSHOT"]
-                 [com.facebook/react "0.9.0.1"]
+                 [com.facebook/react "0.11.1"]
                  [net.polyc0l0r/geschichte "0.1.0-SNAPSHOT"]
                  [markdown-clj "0.9.47"]
 
-                 [weasel "0.2.1"]]
+                 [weasel "0.4.0-SNAPSHOT"]]
 
   :main topiq.core
 
