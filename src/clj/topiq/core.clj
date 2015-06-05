bb8f831ae377f07cfb86d1cc8e3ed4a1c9e9e8f4
Christian Weilbach <christian_weilbach@web.de>
2015-06-05 13:42:09 +0200

    Remove Datomic :/

--- src/clj/topiq/core.clj
+++ src/clj/topiq/core.clj
@@ -28,9 +28,7 @@
             [clojure.core.async :refer [timeout sub chan <!! >!! <! >! go go-loop] :as async]
             [com.ashafa.clutch.utils :as utils]
             [com.ashafa.clutch :refer [couch]]
-            [clojure.tools.logging :refer [info warn error]]
-
-            [datomic.api :as d]))
+            [clojure.tools.logging :refer [info warn error]]))
 
 
 (def server-state (atom nil))
