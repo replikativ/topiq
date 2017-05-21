31fc1804c2ab51a885cf6366308a0f69c49bc8fd
Christian Weilbach <christian@topiq.es>
2017-05-21 17:53:00 +0200

    Add transit-cljs dep.

--- project.clj
+++ project.clj
@@ -26,6 +26,7 @@
                  [org.omcljs/om "0.9.0"]
                  [kioo "0.4.2"]
 
+                 [com.cognitect/transit-cljs "0.8.239" :scope "provided"]
                  [io.replikativ/replikativ "0.2.2"]
                  #_[io.replikativ/kabel-auth "0.1.0-SNAPSHOT"]
                  [com.draines/postal "2.0.1"]
